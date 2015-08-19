544cf8e45e6fa7fd122973837f5d70922ec7b11b
DarynHolmes <DarynHolmes@gmail.com>
2015-08-19 19:53:19 +0100

    Updated the model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817190340) do
+ActiveRecord::Schema.define(version: 20150819183739) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
 end
