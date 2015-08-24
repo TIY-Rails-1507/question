82e8f2c1b31344fefd3e6d172ad4863b5302a8ff
DarynHolmes <DarynHolmes@gmail.com>
2015-08-24 16:28:32 +0100

    Added deleting

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,13 +6,38 @@ class QuestionsController < ApplicationController
 	end
 
 	def show
-		question_id = params[:id]
-		@question = Question.find(question_id)
+		@question = Question.find(params[:id])
 	end
 
 	def edit
-		question_id = params[:id]
-		@question = Question.find(question_id)
+		@question = Question.find(params[:id])
+	end
+
+	def update
+		@question = Question.find(params[:id])
+		@question.update(question_params)
+		redirect_to question_path(@question)
+	end
+
+	def new
+		@question = Question.new
+	end
+
+	def create
+		question = Question.create(question_params)
+		redirect_to question_path(question)
+	end
+
+	def destroy 
+		question = Question.find(params[:id])
+		question.destroy 
+		redirect_to questions_url
+	end
+
+	private 
+
+	def question_params
+		params.require(:question).permit(:title, :body)
 	end
 
 end
